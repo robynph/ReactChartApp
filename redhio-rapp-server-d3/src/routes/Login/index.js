9a2eb0a9e2a3cdb1adf7425a8e0214efb8213043
robyn <robyn@redh.io>
2018-05-15 23:09:13 -0400

    removed app

--- redhio-rapp-server-d3/src/routes/Login/index.js
+++
@@ -1,6 +0,0 @@
-import Login from 'components/Login';
-
-export default {
-  path: '/login',
-  component : Login
-};
--- redhio-rapp-server-d3/src/routes/Signup/index.js
+++
@@ -1,6 +0,0 @@
-import Signup from 'components/Signup';
-
-export default {
-  path: '/signup',
-  component : Signup
-};
